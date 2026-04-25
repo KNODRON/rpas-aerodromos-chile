296d36da5aa3065eb452e16ca7d83ae5aa9a2cdc
KNODRON <juan.jimenezv08@gmail.com>
2026-04-24 20:03:31 -0400

    Create geolocation.js

---
+++ js/geolocation.js
@@ -0,0 +1,30 @@
+function usarMiUbicacion(){
+
+  navigator.geolocation.getCurrentPosition(pos=>{
+
+    let lat = pos.coords.latitude;
+    let lon = pos.coords.longitude;
+
+    let punto = {lat:lat,lng:lon};
+
+    map.setView([lat,lon],13);
+
+    let cercano = null;
+    let min = 999;
+
+    datos.forEach(p=>{
+      let d = distanciaKm(punto,{lat:p.lat,lng:p.lon});
+      if(d<min){
+        min=d;
+        cercano=p;
+      }
+    });
+
+    document.getElementById("resultado").innerHTML =
+      "Más cercano: "+cercano.nombre+
+      "<br>"+min.toFixed(2)+" km"+
+      "<br>"+alertaPorDistancia(min);
+
+  });
+
+}
