ebace01752dd01dcecbc09370cd96bed4a43658e
KNODRON <juan.jimenezv08@gmail.com>
2026-04-24 20:02:10 -0400

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,18 @@
+function distanciaKm(a,b){
+  let R=6371;
+  let dLat=(b.lat-a.lat)*Math.PI/180;
+  let dLon=(b.lng-a.lng)*Math.PI/180;
+
+  let x=Math.sin(dLat/2)**2 +
+        Math.cos(a.lat*Math.PI/180) *
+        Math.cos(b.lat*Math.PI/180) *
+        Math.sin(dLon/2)**2;
+
+  return R*2*Math.atan2(Math.sqrt(x),Math.sqrt(1-x));
+}
+
+function alertaPorDistancia(km){
+  if(km <= 3) return "ALTO RIESGO";
+  if(km <= 5) return "PRECAUCIÓN";
+  return "BAJO";
+}
